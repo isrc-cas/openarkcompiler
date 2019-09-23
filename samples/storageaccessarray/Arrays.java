885030751122e03e22ab257bc25d1a12f7b84fa4
shining <shining@droid.ac.cn>
2019-09-23 20:40:31 +0800

    Add array samples for storage access
    Add array sample for storage access, the source
    code of array is written according the IR design
    doc.

---
+++ samples/storageaccessarray/Arrays.java
@@ -0,0 +1,9 @@
+public class Arrays {
+    public static void main(String[] args) {
+        int a[] = new int[]{1, 2, 3};
+        int i = 2;
+        a[i] = i;
+        System.out.println(a[i]);
+    }
+}
+
